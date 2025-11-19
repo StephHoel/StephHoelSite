afc4a3f0be7ff3c2696ff7872b77d13d368de0d9
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2025-11-19 15:35:32 -0300

    chore(lint): add TypeScript ESLint parser/plugins and apply autofixes

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,3 +1,5 @@
+const tsParser = require('@typescript-eslint/parser')
+
 module.exports = [
   {
     ignores: ['.next', 'node_modules'],
@@ -5,10 +7,18 @@ module.exports = [
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
+      parser: tsParser,
       parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.json',
       },
+      globals: { React: 'writable' },
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      react: require('eslint-plugin-react'),
     },
     rules: {},
   },
