23e02c3886aade478f3144acf108a8358284d421
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2025-11-19 22:52:04 -0300

    feat(ci): enhance GitHub Actions workflow for deployment and testing
    - Add push trigger for the develop branch
    - Implement E2E testing job with Node setup and test execution
    - Add linting job with Node setup and lint execution
    - Update build job to depend on E2E and lint jobs
    - Modify build command to set NEXT_PUBLIC_BASE_PATH
    - Add check for output directory existence
    - Update .gitignore to include out/ directory
    - Update package.json to change start script for serving static files
    - Change base URL in tests to use localhost

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
+const assetPrefix = basePath || ''
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath,
+  assetPrefix,
 }
 
 module.exports = nextConfig
